fa6365a9b492595d221a714be3014974c19d0b07
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-08-25 09:08:20 +0530

    Shoping page app using reducers

---
+++ src/components/store/cart-slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newitem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newitem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newitem.id,
+          price: newitem.price,
+          quantity: 1,
+          totalPrice: newitem.price,
+          name: newitem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newitem.price;
+      }
+    },
+    removeItemToCart() {},
+  },
+});
+
+export default cartSlice;
